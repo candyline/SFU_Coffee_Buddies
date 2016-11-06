a3b5ff29e3534a277115c827a5c5ab84bcd13830
Eton Kan <etonkan91@etons-mac.local>
2016-11-05 23:40:33 -0700

    ShakePage changed

--- SFU Coffee Buddies/SFU Coffee Buddies/ShakePage.swift
+++ SFU Coffee Buddies/SFU Coffee Buddies/ShakePage.swift
@@ -9,12 +9,23 @@
 import Foundation
 import UIKit
 
+struct profile{
+    var id = "581e99fd58b03c3c0831dd98"
+    var meeting = "0"
+    var gender = "0"
+    var password = "0"
+    var user = "0"
+    var text = "0"
+    var unknown = "0"
+}
+
 class ShakePage: UIViewController {
-  
+    
     @IBOutlet weak var shakephone: UILabel!
     @IBOutlet weak var placedinqueue: UILabel!
     @IBOutlet weak var currentlyinqueue: UILabel!
-    override func viewDidLoad() {
+    let Serverhost = "http://127.0.0.1:8080/messages/"
+        override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         shakephone.isHidden = false
@@ -32,15 +43,71 @@ class ShakePage: UIViewController {
         {
             shakephone.isHidden = true
             placedinqueue.isHidden = false
-            //Getting User's gender and ID
+            let userprofile = profile()
+            //let generalhost = URL(string: Serverhost)
+            let appendUserUrl = Serverhost + userprofile.id
+            let userhost = URL(string: appendUserUrl)
+            //Reading from server
+            //MainPage().readDataFromURL()
+            //let linktohost = URL(generalhost)
+            print("before read")
+            //Does it even go in?
+            let task = URLSession.shared.dataTask(with: userhost!) {data, response, error in
+                guard error == nil else {
+                    print ("I have an error") //error)
+                    return
+                }
+                
+                guard let data = data else {
+                    print ("Data is empty")
+                    return
+                }
+                //Why doesn't it work ?????????
+                let json = try! JSONSerialization.jsonObject(with: data, options: [])
+                print (json)
+                print("IaminReadData")
+                //let object = try! JSONSerialization.jsonObject(with: data, options: .allowFragments)
+                //if let dictionary = object as? [String: AnyObject] {
+                //    print(dictionary)
+                //}
+            }
+            print("dataTask read  should be failed")
             
-            //Storing User's gender, ID and current time to the server's data structure
-            let postData = NSMutableData(data: "text=David".data(using: String.Encoding.utf8)!)
-            postData.append("&user=MAOOOO".data(using: String.Encoding.utf8)!)
-            MainPage().postDataToURL(postData: postData as Data)
-           
-            //If number of entries >1 in the server's data structure, matching begin
+            task.resume()
+
             
+            //Look for another user with meeting = one
+            //If found, display the other user's information
+            
+            //else put user on the queue and let the other user look for you
+            //Getting User's ID
+            //Storing user able to meet on server data
+            
+            //let appendUserUrl = Serverhost + userprofile.id
+            //let userhost = URL(string: appendUserUrl)
+            //let appendUsertoHost = generalhost + userprofile.id as URL
+            //let userHost = URL(fileURLWithPath: appendUsertoHost)
+
+            let postData = NSMutableData(data: "meeting=1".data(using: String.Encoding.utf8)!)
+            //postData.append("&user=M".data(using: String.Encoding.utf8)!)
+            //MainPage().postDataToURL(postData: postData as Data)
+            //Posting it to localhost:8080/messages/id
+            let request = NSMutableURLRequest(url: userhost!,                                          cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10.0)
+            
+            request.httpMethod = "POST"
+            request.httpBody = postData as Data
+            
+            let session = URLSession.shared
+            let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
+                if (error != nil) {
+                    print(error)
+                } else {
+                    let httpResponse = response as? HTTPURLResponse
+                    print(httpResponse)
+                }
+            })
+            
+            dataTask.resume()
         }
     }
     
