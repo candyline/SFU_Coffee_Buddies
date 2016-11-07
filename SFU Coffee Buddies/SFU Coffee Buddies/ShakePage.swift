70667a035839f3c8279f684c6408e64963b1520a
Eton Kan <etonkan91@etons-mac.local>
2016-11-06 17:40:36 -0800

    Could be used for Video

--- SFU Coffee Buddies/SFU Coffee Buddies/ShakePage.swift
+++ SFU Coffee Buddies/SFU Coffee Buddies/ShakePage.swift
@@ -13,7 +13,8 @@ import SwiftyJSON
 
 
 struct profile{
-    var id = "581e99fd58b03c3c0831dd98"
+    public
+    var id = "0"
     var meeting = "0"
     var gender = "0"
     var password = "0"
@@ -24,30 +25,49 @@ struct profile{
 
 class ShakePage: UIViewController {
     
+    //Declare all the variables used in the storyboard
+    @IBOutlet weak var TargetBioLabel: UILabel!
+    @IBOutlet weak var TargetGenderLabel: UILabel!
+    @IBOutlet weak var TargetNameLabel: UILabel!
+    @IBOutlet weak var TargetBioDisplay: UILabel!
+    @IBOutlet weak var TargetGenderDisplay: UILabel!
     @IBOutlet weak var shakephone: UILabel!
     @IBOutlet weak var placedinqueue: UILabel!
     @IBOutlet weak var currentlyinqueue: UILabel!
+    @IBOutlet weak var matched: UILabel!
+    @IBOutlet weak var TargetNameDisplay: UILabel!
+    //Default Server Address
     let Serverhost = "http://127.0.0.1:8080/messages/"
-    var jsonArray: NSMutableArray? //added
         override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         shakephone.isHidden = false
         placedinqueue.isHidden = true
         currentlyinqueue.isHidden = true
+        matched.isHidden = true
+        TargetNameDisplay.isHidden = true
+        TargetGenderDisplay.isHidden = true
+        TargetBioDisplay.isHidden = true
+        TargetBioLabel.isHidden = true
+        TargetNameLabel.isHidden = true
+        TargetGenderLabel.isHidden = true
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    
+    //A function that detects shake motion either match or put the current user on the queue
+    //Author: Eton Kan
+    //Last Modify: Nov 6,2016
     override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
         if event?.subtype == UIEventSubtype.motionShake
         {
             shakephone.isHidden = true
             placedinqueue.isHidden = false
-            let userprofile = profile()
+            var userProfile = profile()
+            var targetProfile = profile()
+            let yesMeeting = "true"
+            //Getting information from user
             Alamofire.request(Serverhost).responseJSON {
                response in
                     //print(response.request)  // original URL request
@@ -55,74 +75,113 @@ class ShakePage: UIViewController {
                     //print(response.data)     // server data
                     //print(response.result)   // result of response serialization
                 
-                    let testing = JSON(response.result.value!)
-                    print(testing)
-                    print(testing.count)
-                for index in 0 ... testing.count {
-                    if let username  = testing[index]["user"].string{
-                        if username == userprofile.user{
-                            print("Yes, it work, i am etonkan")}
+                    let dataBaseArray = JSON(response.result.value!)
+                //Search the user inside the testing array
+                for index in 0 ... dataBaseArray.count {
+                    if let user  = dataBaseArray[index]["user"].string{
+                        if user == userProfile.user{
+                            //Enter the user's information into a profile struct
+                            if let id = dataBaseArray[index]["_id"].string{
+                                userProfile.id = id
+                                print(userProfile.id)
+                            }
+                            if let gender = dataBaseArray[index]["gender"].string{
+                                userProfile.gender = gender
+                                //print(userProfile.gender)
+                            }
+                            if let password = dataBaseArray[index]["password"].string{
+                                userProfile.password = password
+                                //print(userProfile.password)
+                            }
+                            if let text = dataBaseArray[index]["text"].string{
+                                userProfile.text = text
+                                //print(userProfile.text)
+                            }
+                            if let meeting = dataBaseArray[index]["meeting"].string{
+                                userProfile.meeting = meeting
+                                //print(userProfile.meeting)
+                            }
+                        }
                         else{
-                        print("No, it doesn't work")}
+                        print("User NOT Found")}
                     }
                 }
-                
-            }
-        
-        
-        
-            //let generalhost = URL(string: Serverhost)
-            let appendUserUrl = Serverhost + userprofile.id
-            let userhost = URL(string: appendUserUrl)
-            //Reading from server
-            //MainPage().readDataFromURL()
-            //let linktohost = URL(generalhost)
-            print("before read")
-/*
-            //Does it even go in?
-            let task = URLSession.shared.dataTask(with: userhost!) {data, response, error in
-                guard error == nil else {
-                    print ("I have an error") //error)
-                    return
+                //Look for another user who want to meet
+                //If found, display the other user's information
+                for index in 0 ... dataBaseArray.count {
+                    if let target_user  = dataBaseArray[index]["user"].string{
+                        if let target_meeting = dataBaseArray[index]["meeting"].string{
+                            if target_user != userProfile.user && target_meeting == yesMeeting{
+                                //Enter the target user's information into a profile struct
+                                if let id = dataBaseArray[index]["_id"].string{
+                                    targetProfile.id = id
+                                    print(targetProfile.id)
+                                }
+                                if let gender = dataBaseArray[index]["gender"].string{
+                                    targetProfile.gender = gender
+                                    print(targetProfile.gender)
+                                }
+                                if let password = dataBaseArray[index]["password"].string{
+                                    targetProfile.password = password
+                                    print(targetProfile.password)
+                                }
+                                if let text = dataBaseArray[index]["text"].string{
+                                    targetProfile.text = text
+                                    print(targetProfile.text)
+                                }
+                                if let meeting = dataBaseArray[index]["meeting"].string{
+                                    targetProfile.meeting = meeting
+                                    print(targetProfile.meeting)
+                                }
+                                //.put change targetProfile.meeting to false
+                                
+                                //Go to the profile screen and display target user information
+                                self.placedinqueue.isHidden = true
+                                self.matched.isHidden = false
+                                self.TargetNameDisplay.isHidden = false
+                                self.TargetGenderDisplay.isHidden = false
+                                self.TargetBioDisplay.isHidden = false
+                                self.TargetBioLabel.isHidden = false
+                                self.TargetNameLabel.isHidden = false
+                                self.TargetGenderLabel.isHidden = false
+                                self.TargetNameLabel.text = targetProfile.user
+                                self.TargetGenderLabel.text = targetProfile.gender
+                                self.TargetBioLabel.text = targetProfile.text
+                                return
+                            }
+                        }
+                        else{
+                            print("NO Match Found")}
+                    }
                 }
-                
-                guard let data = data else {
-                    print ("Data is empty")
-                    return
-                }
-                //Why doesn't it work ?????????
-                let json = try! JSONSerialization.jsonObject(with: data, options: [])
-                print (json)
-                print("IaminReadData")
-                //let object = try! JSONSerialization.jsonObject(with: data, options: .allowFragments)
-                //if let dictionary = object as? [String: AnyObject] {
-                //    print(dictionary)
-                //}
-            }
-            print("dataTask read  should be failed")
-            
-            task.resume()
-*/
-            
-            //Look for another user with meeting = one
-            //If found, display the other user's information
-            
-            //else put user on the queue and let the other user look for you
-            //Getting User's ID
-            //Storing user able to meet on server data
-            
-            //let appendUserUrl = Serverhost + userprofile.id
-            //let userhost = URL(string: appendUserUrl)
-            //let appendUsertoHost = generalhost + userprofile.id as URL
-            //let userHost = URL(fileURLWithPath: appendUsertoHost)
 
-            let postData = NSMutableData(data: "meeting=1".data(using: String.Encoding.utf8)!)
-            //postData.append("&user=M".data(using: String.Encoding.utf8)!)
-            //MainPage().postDataToURL(postData: postData as Data)
-            //Posting it to localhost:8080/messages/id
-            let request = NSMutableURLRequest(url: userhost!,                                          cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10.0)
             
-            request.httpMethod = "POST"
+            //Change meeting to true on DataBase and let the other user look for you
+            let headers = [
+                    "content-type": "application/x-www-form-urlencoded",
+                    "authorization": "Basic Og==",
+                    "cache-control": "no-cache",
+                    "postman-token": "6f01f645-96bb-f323-a5db-4abe6ba03694"
+            ]
+            print(userProfile.id)
+            let appendedUserUrl = self.Serverhost + userProfile.id
+            let userUrl = NSURL(string: appendedUserUrl)
+            let userText = "text="+userProfile.text
+                print(userText)
+            let userUser = "&user="+userProfile.user
+                print(userUser)
+            //hard coded
+            var postData = NSMutableData(data: "text=SFU".data(using: String.Encoding.utf8)!)
+            postData.append("&user=she".data(using: String.Encoding.utf8)!)
+            postData.append("&password=789".data(using: String.Encoding.utf8)!)
+            postData.append("&gender=male".data(using: String.Encoding.utf8)!)
+            postData.append("&meeting=true".data(using: String.Encoding.utf8)!)
+            
+            var request = NSMutableURLRequest(url: userUrl! as URL,
+                                              cachePolicy: .useProtocolCachePolicy,
+                                              timeoutInterval: 10.0)
+            request.httpMethod = "PUT"
+            request.allHTTPHeaderFields = headers
             request.httpBody = postData as Data
             
             let session = URLSession.shared
@@ -136,7 +195,15 @@ class ShakePage: UIViewController {
             })
             
             dataTask.resume()
+            
+            
+            
+            
+            
+            
+            
         }
     }
     
+}
 }
