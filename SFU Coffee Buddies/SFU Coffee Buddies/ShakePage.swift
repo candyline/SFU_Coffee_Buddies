c255bb953c558cfee99966968f0cfd3106dea215
Eton Kan <etonkan91@etons-mac.local>
2016-11-07 01:46:06 -0800

    Comment Changed

--- SFU Coffee Buddies/SFU Coffee Buddies/ShakePage.swift
+++ SFU Coffee Buddies/SFU Coffee Buddies/ShakePage.swift
@@ -1,8 +1,21 @@
 //
-//  ShakePage.swift
-//  SFU Coffee Buddies
+//  File Name: ShakePage.swift
+//  Project Name: SFU Coffee Buddies
+//  Team Name: Group3Genius (G3G)
+//  Author: Eton Kan
+//  Creation Date: Oct 28, 2016
+//  List of Changes:
+//  V1.0: Created by Eton Kan
+//  V1.1: motionEnded() detect user shaking motion
+//  V1.2: labels are added
+//  V1.3: Read and PUT to database are implemented
 //
-//  Created by Eton Kan on 2016-10-28.
+//  Last Modified Author: Eton Kan
+//  Last Modified Date: Nov 6, 2016
+//
+//  List of Bugs:
+//  motionEnded(): 1) it will crash the database if zero entries are in it
+//                 2) types used for variables are not efficient
 //  Copyright © 2016 Eton Kan. All rights reserved.
 //
 
@@ -11,8 +24,11 @@ import UIKit
 import Alamofire
 import SwiftyJSON
 
-
-struct profile{
+//public struct that is used to save information from database
+//Author: Eton Kan
+//Last Modifty: Nov 6,2016
+//Known Bugs: none
+struct Profile{
     public
     var id = "0"
     var meeting = "0"
@@ -23,39 +39,50 @@ struct profile{
     var unknown = "0"
 }
 
+//This classs is used to detect shake motion and communicate with user on the status
+//placed on the queue
+//Author: Eton Kan
+//Last Modifty: Nov 6,2016
+//Known Bugs:
+//In motionEnded():
+//            1) it will crash the database if zero entries are in it
+//            2) types used for variables are not efficient
 class ShakePage: UIViewController {
     
-    //Declare all the variables used in the storyboard
-    @IBOutlet weak var TargetBioLabel: UILabel!
-    @IBOutlet weak var TargetGenderLabel: UILabel!
-    @IBOutlet weak var TargetNameLabel: UILabel!
-    @IBOutlet weak var TargetBioDisplay: UILabel!
-    @IBOutlet weak var TargetGenderDisplay: UILabel!
-    @IBOutlet weak var shakephone: UILabel!
-    @IBOutlet weak var placedinqueue: UILabel!
-    @IBOutlet weak var currentlyinqueue: UILabel!
+    //Declare all variables used in the storyboard
+    @IBOutlet weak var targetBioLabel: UILabel!
+    @IBOutlet weak var targetGenderLabel: UILabel!
+    @IBOutlet weak var targetNameLabel: UILabel!
+    @IBOutlet weak var targetBioDisplay: UILabel!
+    @IBOutlet weak var targetGenderDisplay: UILabel!
+    @IBOutlet weak var shakePhone: UILabel!
+    @IBOutlet weak var placedInQueue: UILabel!
+    @IBOutlet weak var currentlyInQueue: UILabel!
     @IBOutlet weak var matched: UILabel!
-    @IBOutlet weak var TargetNameDisplay: UILabel!
-    //Default Server Address
+    @IBOutlet weak var targetNameDisplay: UILabel!
+    //Default Server Address (localhost)
     let Serverhost = "http://127.0.0.1:8080/messages/"
+   
+    //Initilize the page when user enter the page
+    //Author: Eton Kan
+    //Last Modify: Nov 6,2016
+    //Known Bugs: none
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        shakephone.isHidden = false
-        placedinqueue.isHidden = true
-        currentlyinqueue.isHidden = true
+        //Instructing the user to shake their device
+        shakePhone.isHidden = false
+        //Hiding the labels from the user
+        placedInQueue.isHidden = true
+        currentlyInQueue.isHidden = true
         matched.isHidden = true
-        TargetNameDisplay.isHidden = true
-        TargetGenderDisplay.isHidden = true
-        TargetBioDisplay.isHidden = true
-        TargetBioLabel.isHidden = true
-        TargetNameLabel.isHidden = true
-        TargetGenderLabel.isHidden = true
+        targetNameDisplay.isHidden = true
+        targetGenderDisplay.isHidden = true
+        targetBioDisplay.isHidden = true
+        targetBioLabel.isHidden = true
+        targetNameLabel.isHidden = true
+        targetGenderLabel.isHidden = true
     }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
+
     //A function that detects shake motion either match or put the current user on the queue
     //Author: Eton Kan
     //Last Modify: Nov 6,2016
@@ -66,30 +93,34 @@ class ShakePage: UIViewController {
     override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
         if event?.subtype == UIEventSubtype.motionShake
         {
-            shakephone.isHidden = true
-            placedinqueue.isHidden = false
-            var userProfile = profile()
-            var targetProfile = profile()
+            //Showing the user that shake is detected
+            shakePhone.isHidden = true
+            placedInQueue.isHidden = false
+            //Initialize Variabes
+            var userProfile = Profile()
+            var targetProfile = Profile()
             let yesMeeting = "true"
             userProfile.user = globalname
             var error = "1"
-            //Getting information from user
+            //Getting user information from database
             Alamofire.request(Serverhost).responseJSON {
                 response in
+                //Used for testing outputs
                 //print(response.request)  // original URL request
                 //print(response.response) // HTTP URL response
                 //print(response.data)     // server data
                 //print(response.result)   // result of response serialization
                 
                 let dataBaseArray = JSON(response.result.value!)
+                
                 //Search the user inside the testing array
                 for index in 0 ... dataBaseArray.count {
                     if let user  = dataBaseArray[index]["user"].string{
                         if user == userProfile.user{
-                            //Enter the user's information into a profile struct
+                            //Enter the user's information into the Profile struct
                             if let id = dataBaseArray[index]["_id"].string{
                                 userProfile.id = id
-                                print(userProfile.id)
+                                //print(userProfile.id)
                             }
                             if let gender = dataBaseArray[index]["gender"].string{
                                 userProfile.gender = gender
@@ -111,7 +142,6 @@ class ShakePage: UIViewController {
                             break;
                         }
                     }
-                    //error = "0"
                 }
                 if error == "1"{
                     print("User NOT Found")
@@ -119,53 +149,54 @@ class ShakePage: UIViewController {
                     return
                 }
                 
-                //Look for another user who want to meet
-                //If found, display the other user's information
+                //Look for other users who want to meet
+                //If found, display the other user's information for current user
                 for index in 0 ... dataBaseArray.count {
+                    //Looking for user that is able to meet
                     if let target_user  = dataBaseArray[index]["user"].string{
                         if let target_meeting = dataBaseArray[index]["meeting"].string{
                             if target_user != userProfile.user && target_meeting == yesMeeting{
                                 //Enter the target user's information into a profile struct
                                 if let id = dataBaseArray[index]["_id"].string{
                                     targetProfile.id = id
-                                    print(targetProfile.id)
+                                    //print(targetProfile.id)
                                 }
                                 if let gender = dataBaseArray[index]["gender"].string{
                                     targetProfile.gender = gender
-                                    print(targetProfile.gender)
+                                    //print(targetProfile.gender)
                                 }
                                 if let password = dataBaseArray[index]["password"].string{
                                     targetProfile.password = password
-                                    print(targetProfile.password)
+                                    //print(targetProfile.password)
                                 }
                                 if let text = dataBaseArray[index]["text"].string{
                                     targetProfile.text = text
-                                    print(targetProfile.text)
+                                    //print(targetProfile.text)
                                 }
                                 if let meeting = dataBaseArray[index]["meeting"].string{
                                     targetProfile.meeting = meeting
-                                    print(targetProfile.meeting)
+                                    //print(targetProfile.meeting)
                                 }
                                 if let userName = dataBaseArray[index]["user"].string{
                                     targetProfile.user = userName
-                                    print(targetProfile.user)
+                                    //print(targetProfile.user)
                                 }
-                                //.put change targetProfile.meeting to false
                                 
-                                //Go to the profile screen and display target user information
-                                self.placedinqueue.isHidden = true
+                                //Display target user information on the ShakePage
+                                self.placedInQueue.isHidden = true
                                 self.matched.isHidden = false
-                                self.TargetNameDisplay.isHidden = false
-                                self.TargetGenderDisplay.isHidden = false
-                                self.TargetBioDisplay.isHidden = false
-                                self.TargetBioLabel.isHidden = false
-                                self.TargetNameLabel.isHidden = false
-                                self.TargetGenderLabel.isHidden = false
-                                self.TargetNameLabel.text = targetProfile.user
-                                self.TargetGenderLabel.text = targetProfile.gender
-                                self.TargetBioLabel.text = targetProfile.text
+                                self.targetNameDisplay.isHidden = false
+                                self.targetGenderDisplay.isHidden = false
+                                self.targetBioDisplay.isHidden = false
+                                self.targetBioLabel.isHidden = false
+                                self.targetNameLabel.isHidden = false
+                                self.targetGenderLabel.isHidden = false
+                                self.targetNameLabel.text = targetProfile.user
+                                self.targetGenderLabel.text = targetProfile.gender
+                                self.targetBioLabel.text = targetProfile.text
                                 
                                 //Updateing Target meeting information
+                                //.put change targetProfile.meeting to false
                                 let headers = [
                                     "content-type": "application/x-www-form-urlencoded",
                                     "authorization": "Basic Og==",
@@ -176,14 +207,14 @@ class ShakePage: UIViewController {
                                 let appendedUserUrl = self.Serverhost + targetProfile.id
                                 let userUrl = NSURL(string: appendedUserUrl)
                                 let userUser = "&user="+targetProfile.user
-                                print(userUser)
+                                //print(userUser)
                                 let userPass = "&password="+targetProfile.password
-                                print(userPass)
+                                //print(userPass)
                                 let userGender = "&gender="+targetProfile.gender
-                                print(userGender)
+                                //print(userGender)
                                 let userText = "&text="+targetProfile.text
-                                print(userText)
-                                //Sending the information of the user to Database
+                                //print(userText)
+                                //Modifying the target user meeting variable to false
                                 var postData = NSMutableData(data: "meeting=false".data(using: String.Encoding.utf8)!)
                                 postData.append(userUser.data(using: String.Encoding.utf8)!)
                                 postData.append(userPass.data(using: String.Encoding.utf8)!)
@@ -196,7 +227,7 @@ class ShakePage: UIViewController {
                                 request.httpMethod = "PUT"
                                 request.allHTTPHeaderFields = headers
                                 request.httpBody = postData as Data
-                                
+                                //Sending request using http method
                                 let session = URLSession.shared
                                 let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
                                     if (error != nil) {
@@ -216,7 +247,7 @@ class ShakePage: UIViewController {
                 }
                 print("NO Match Found")
                 print("Putting User ON Queue")
-                //Change meeting to true on DataBase and let the other user look for you
+                //Change user's meeting to true on DataBase and let the other users look for you
                 let headers = [
                     "content-type": "application/x-www-form-urlencoded",
                     "authorization": "Basic Og==",
@@ -247,7 +278,7 @@ class ShakePage: UIViewController {
                 request.httpMethod = "PUT"
                 request.allHTTPHeaderFields = headers
                 request.httpBody = postData as Data
-                
+                //Sending request using http method
                 let session = URLSession.shared
                 let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
                     if (error != nil) {
