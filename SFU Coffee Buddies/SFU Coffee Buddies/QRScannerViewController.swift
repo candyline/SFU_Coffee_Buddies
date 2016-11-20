9ab842b7d4bca39fbe32a7e872e63dac6314d8d8
Daniel Tan <danieltan@daniels-mac.local>
2016-11-20 00:32:00 -0800

    Added QR Code Scanner

--- SFU Coffee Buddies/SFU Coffee Buddies/QRScannerViewController.swift
+++ SFU Coffee Buddies/SFU Coffee Buddies/QRScannerViewController.swift
@@ -5,20 +5,100 @@
 //  Created by Daniel Tan on 2016-11-13.
 //  Copyright © 2016 CMPT275-3. All rights reserved.
 //
+//  Team : Group3Genius
+//
+//  Changelog:
+//      -File Created and Fundamental Functions Implemented
 
 import UIKit
+import AVFoundation
 
-class QRScannerViewController: UIViewController {
+class QRScannerViewController: UIViewController,
+                               AVCaptureMetadataOutputObjectsDelegate{
 
-    override func viewDidLoad() {
+    // Variables and Outlets
+    @IBOutlet weak var promptlbl: UITextView!
+    @IBOutlet weak var lblQRCodeLabel: UILabel!
+    @IBOutlet weak var lblQRCodeResult: UILabel!
+    var objCaptureSession:AVCaptureSession?
+    var objCaptureVideoPreviewLayer:AVCaptureVideoPreviewLayer?
+    var vwQRCode:UIView?
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.configureVideoCapture()
+        self.addVideoPreviewLayer()
+        self.initializeQRView()
+        
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func configureVideoCapture()
+    {
+        let objCaptureDevice = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+        var error:NSError?
+        let objCaptureDeviceInput: AnyObject!
+        do {
+            objCaptureDeviceInput = try AVCaptureDeviceInput(device: objCaptureDevice) as AVCaptureDeviceInput
+        } catch let error1 as NSError {
+            error = error1
+            objCaptureDeviceInput = nil
+        }
+        if (error != nil) {
+            let alertView:UIAlertView = UIAlertView(title: "Device Error", message:"Device not Supported for this Application", delegate: nil, cancelButtonTitle: "Ok Done")
+            alertView.show()
+            return
+        }
+        objCaptureSession = AVCaptureSession()
+        objCaptureSession?.addInput(objCaptureDeviceInput as! AVCaptureInput)
+        let objCaptureMetadataOutput = AVCaptureMetadataOutput()
+        objCaptureSession?.addOutput(objCaptureMetadataOutput)
+        objCaptureMetadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+        objCaptureMetadataOutput.metadataObjectTypes = [AVMetadataObjectTypeQRCode]
+    }
+
+    
+    func addVideoPreviewLayer()
+    {
+        objCaptureVideoPreviewLayer = AVCaptureVideoPreviewLayer(session: objCaptureSession)
+        objCaptureVideoPreviewLayer?.videoGravity = AVLayerVideoGravityResizeAspectFill
+        objCaptureVideoPreviewLayer?.frame = view.layer.bounds
+        self.view.layer.addSublayer(objCaptureVideoPreviewLayer!)
+        objCaptureSession?.startRunning()
+    }
+    
+    func initializeQRView() {
+        vwQRCode = UIView()
+        vwQRCode?.layer.borderColor = UIColor.red.cgColor
+        vwQRCode?.layer.borderWidth = 5
+        self.view.addSubview(vwQRCode!)
+        self.view.bringSubview(toFront: vwQRCode!)
+    }
+    
+    // Delegate handling function
+    func captureOutput(captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [AnyObject]!, fromConnection connection: AVCaptureConnection!) {
+        if metadataObjects == nil || metadataObjects.count == 0 {
+            vwQRCode?.frame = CGRect.zero
+            lblQRCodeResult.text = "NO QRCode text detected"
+            return
+        }
+        let objMetadataMachineReadableCodeObject = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
+        if objMetadataMachineReadableCodeObject.type == AVMetadataObjectTypeQRCode
+        {
+            let objBarCode = objCaptureVideoPreviewLayer?.transformedMetadataObject(for: objMetadataMachineReadableCodeObject as AVMetadataMachineReadableCodeObject) as! AVMetadataMachineReadableCodeObject
+            vwQRCode?.frame = objBarCode.bounds;
+            if objMetadataMachineReadableCodeObject.stringValue != nil
+            {
+                lblQRCodeResult.text = objMetadataMachineReadableCodeObject.stringValue
+            }
+            // Do check with database here
+        }
     }
     
 
