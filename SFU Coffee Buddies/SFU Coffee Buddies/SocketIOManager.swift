f60dc716e186d087afb64c267c45cef397e3c203
Frank Su <fpsu@sfu.ca>
2016-12-03 17:36:42 -0800

    added socket.io in carthage, run 'carthage update --platform ios,macosx' to update

---
+++ SFU Coffee Buddies/SFU Coffee Buddies/SocketIOManager.swift
@@ -0,0 +1,17 @@
+//
+//  SocketIOManager.swift
+//  SFU Coffee Buddies
+//
+//  Created by Frank Su on 2016-12-03.
+//  Copyright © 2016 CMPT275-3. All rights reserved.
+//
+/*
+import UIKit
+import SocketIO
+
+class SocketIOManager: NSObject {
+    static let sharedInstance = SocketIOManager()
+    var socket: SocketIOClient = SocketIOClient(socketURL: NSURL(string: "http://207.23.164.121:3000")! as URL)
+    
+}
+*/
