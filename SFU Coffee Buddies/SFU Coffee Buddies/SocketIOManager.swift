d5e65c82c0d18159e2fb6b1a9b10fae0940ef24f
Frank Su <fpsu@sfu.ca>
2016-12-04 20:32:12 -0800

    re added socket stuff cuz they disappearred

--- SFU Coffee Buddies/SFU Coffee Buddies/SocketIOManager.swift
+++ SFU Coffee Buddies/SFU Coffee Buddies/SocketIOManager.swift
@@ -1,17 +1,52 @@
-//
-//  SocketIOManager.swift
-//  SFU Coffee Buddies
-//
-//  Created by Frank Su on 2016-12-03.
-//  Copyright © 2016 CMPT275-3. All rights reserved.
-//
-/*
 import UIKit
-import SocketIO
 
 class SocketIOManager: NSObject {
+    // so we can call this anywhere in the code
     static let sharedInstance = SocketIOManager()
-    var socket: SocketIOClient = SocketIOClient(socketURL: NSURL(string: "http://207.23.164.121:3000")! as URL)
+    
+    override init() {
+        super.init()
+    } //http://guarded-shore-21847.herokuapp.com
+    // basic class SocketIOClient enable us to send  and receive message from the server
+    var socket: SocketIOClient = SocketIOClient(socketURL: NSURL(string: "http://192.168.1.73:3000")! as URL)
+    
+    func establishConnection() {
+        socket.connect()
+    }
+    
+    func closeConnection() {
+        socket.disconnect()
+    }
+    
+    func connectToServerWithNickname(nickname: String, completionHandler: @escaping (_ userList: [[String: AnyObject]]?) -> Void) {
+        socket.emit("connectUser", nickname)
+        
+        
+        socket.on("userList") { ( dataArray, ack) -> Void in
+            completionHandler(dataArray[0] as? [[String: AnyObject]])
+        }
+    }
+    
+    func exitChatWithNickname(nickname: String, completionHandler: () -> Void) {
+        socket.emit("exitUser", nickname)
+        completionHandler()
+    }
+    
+    //chatting
+    func sendMessage(message: String, withNickname nickname: String) {
+        socket.emit("chatMessage", nickname, message)
+    }
+    // sends the name  + message + date to client
+    func getChatMessage(completionHandler: @escaping (_ messageInfo: [String: AnyObject]) -> Void) {
+        socket.on("newChatMessage") { (dataArray, socketAck) -> Void in
+            var messageDictionary = [String: AnyObject]()
+            messageDictionary["nickname"] = dataArray[0] as! String as AnyObject?
+            messageDictionary["message"] = dataArray[1] as! String as AnyObject?
+            messageDictionary["date"] = dataArray[2] as! String as AnyObject?
+            
+            completionHandler(messageDictionary)
+        }
+    }
+    
     
 }
-*/
